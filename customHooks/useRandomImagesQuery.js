7dab9512d5d19cf0a19926a56698389428738644
prasanv <prasan.venkatb@gmail.com>
2024-06-06 10:11:03 -0700

    major refactoring

--- customHooks/useRandomImagesQuery.js
+++ customHooks/useRandomImagesQuery.js
@@ -1,8 +1,9 @@
 import { useQuery } from "react-query";
 
 const fetchRqsuperheros = async ()=> {
-  const res = await fetch("https://foodish-api.herokuapp.com/api/");
+  const res = await fetch("https://api.nekosapi.com/v3/images/random");
   const data = await res.json();
+  console.log(data)
   return data;
 }
 
--- pages/mutationsResponseData.js
+++ pages/mutationsResponseData.js
@@ -1,72 +1,115 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { Button, Text, Box, FormLabel, FormControl, Input } from '@chakra-ui/react'
-import React, { useState } from 'react'
-import { addSuperHeroesFromMutationResponse, getSuperHeroes } from "../customHooks/useSuperHeroes";
+import {
+  Button,
+  Text,
+  Box,
+  FormLabel,
+  FormControl,
+  Input,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  addSuperHeroesFromMutationResponse,
+  getSuperHeroes,
+} from "../customHooks/useSuperHeroesMutations";
 
 export default function mutationsResponseData() {
-  const [superheroName, setSuperheroName] = useState('');
-  const [alterEgoName, setAlterEgoName] = useState('');
-  
+  const [superheroName, setSuperheroName] = useState("");
+  const [alterEgoName, setAlterEgoName] = useState("");
+
   const result = getSuperHeroes();
 
   const mutateResult = addSuperHeroesFromMutationResponse();
 
   const submitHandler = () => {
-      const hero = {name: superheroName, alterEgo: alterEgoName};
-      mutateResult.mutate(hero);
-      setSuperheroName('');
-      setAlterEgoName('');
-  }
+    const hero = { name: superheroName, alterEgo: alterEgoName };
+    mutateResult.mutate(hero);
+    setSuperheroName("");
+    setAlterEgoName("");
+  };
 
   return (
     <div>
-      <Text fontSize='3xl' fontWeight='600'> Mutations </Text>
-      
-      <Box sx={{border: '2px solid grey', padding:'20px', margin:'15px', borderRadius:'15px'}}>
-        <Text fontSize='2xl' textDecoration='underline'>Superheroes list</Text>  
+      <Text fontSize="3xl" fontWeight="600">
+        {" "}
+        Mutations{" "}
+      </Text>
+
+      <Box
+        sx={{
+          border: "2px solid grey",
+          padding: "20px",
+          margin: "15px",
+          borderRadius: "15px",
+        }}
+      >
+        <Text fontSize="2xl" textDecoration="underline">
+          Superheroes list
+        </Text>
         <div>
-          {result.data?.map(item => {
-            return (<p key={item.id}>{item.id}. {item.name}</p>)
+          {result.data?.map((item) => {
+            return (
+              <p key={item.id}>
+                {item.id}. {item.name}
+              </p>
+            );
           })}
         </div>
-        <Button 
-          m={2} 
+        <Button
+          m={2}
           onClick={result.refetch}
           isLoading={result.isLoading || result.isFetching}
-        >Fetch Heroes</Button>
+        >
+          Fetch Heroes
+        </Button>
       </Box>
-      <Box sx={{border: '2px solid grey', padding:'20px', margin:'15px', borderRadius:'15px'}}>
-        <Text fontSize='2xl' textDecoration='underline'>Add Superheroes to the list</Text>  
+      <Box
+        sx={{
+          border: "2px solid grey",
+          padding: "20px",
+          margin: "15px",
+          borderRadius: "15px",
+        }}
+      >
+        <Text fontSize="2xl" textDecoration="underline">
+          Add Superheroes to the list
+        </Text>
         <form>
           <FormControl m={2}>
-              <FormLabel htmlFor='superhero' id='superhero-label'>Superhero Name </FormLabel> 
-              <Input 
-                id='superhero'
-                name='superhero' 
-                type='text' 
-                value={superheroName} 
-                onChange={(e) => setSuperheroName(e.target.value)}
-              ></Input>
+            <FormLabel htmlFor="superhero" id="superhero-label">
+              Superhero Name{" "}
+            </FormLabel>
+            <Input
+              id="superhero"
+              name="superhero"
+              type="text"
+              value={superheroName}
+              onChange={(e) => setSuperheroName(e.target.value)}
+            ></Input>
           </FormControl>
           <FormControl m={2}>
-              <FormLabel htmlFor='alterEgo' id='alterEgo-label'>AlterEgo Name </FormLabel> 
-              <Input 
-                name='alterEgo' 
-                type='text' 
-                value={alterEgoName} 
-                onChange={(e) => setAlterEgoName(e.target.value)}
-                id='alterEgoName'
-              ></Input>
+            <FormLabel htmlFor="alterEgo" id="alterEgo-label">
+              AlterEgo Name{" "}
+            </FormLabel>
+            <Input
+              name="alterEgo"
+              type="text"
+              value={alterEgoName}
+              onChange={(e) => setAlterEgoName(e.target.value)}
+              id="alterEgoName"
+            ></Input>
           </FormControl>
-          <Button 
-            m={2} 
+          <Button
+            m={2}
             onClick={submitHandler}
             isLoading={mutateResult.isLoading}
-          >{(mutateResult.isIdle && "submit") || (mutateResult.isSuccess && "Done") || (mutateResult.isError && "Try Again")}
+          >
+            {(mutateResult.isIdle && "submit") ||
+              (mutateResult.isSuccess && "Done") ||
+              (mutateResult.isError && "Try Again")}
           </Button>
         </form>
       </Box>
-      
     </div>
-  )
+  );
 }
